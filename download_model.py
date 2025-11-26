8add34e0546ab91be10bf65c66cc49d3d3576959
lst11111 <lst13841623179@gmail.com>
2025-11-26 16:52:49 +0800

    修改downloade_model

--- download_model.py
+++ download_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 ##下载模型权重
 model_dir = snapshot_download(
-                            repo_id="hfl/chinese-bert-wwm",
-                            local_dir="/home/lisongtao/ner_basic_demo/pre_model/chinese-bert-wwm",
-                            ignore_patterns=["*.h5", "*.ot", "tf_model*"] )
+                            repo_id="hfl/chinese-bert-wwm",##huggingface中模型的名称
+                            local_dir="/home/lisongtao/ner_basic_demo/pre_model/chinese-bert-wwm",##下载的位置
+                            ignore_patterns=["*.h5", "*.ot", "tf_model*"] )##忽略的文件
 print(f"模型已经下载到: {model_dir}")
